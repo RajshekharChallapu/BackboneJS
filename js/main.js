91143eac2925ac47ce71174b1ea637ba6fbbd5a3
CR <rajshekhar@protonmail.com>
2018-02-27 23:07:52 -0600

    Collections..where,filter and findWhere methods

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
-
-//Collections
+// Collections
 
 var Song = Backbone.Model.extend();
-
 var Songs = Backbone.Collection.extend({
-  model: Song
+  model:Song
+})
+
+var songs = new Songs();
+
+// by using add method, we can specifiy the index number dat where to add
+songs.add(new Song({ title: " Song 1", genre:" Jazz", downloads: 100}),{at:0});
+
+songs.push(new Song({ title: " Song 2", genre:" Jazz", downloads: 160}));
+
+var jazzSongs = songs.where({genre: "Jazz"});
+
+var firstJazzSong = songs.findWhere({genre: "Jazz"});
+
+console.log("Jazz Songs", jazzSongs);
+
+console.log("First Jazz Song", firstJazzSong);
+
+var filteredSongs = songs.where({genre: "Jazz", title: "Song 2"});
+console.log("Filtered Songs", filteredSongs);
+
+var topDownloads =songs.filter(function(song){
+  return song.get("downloads")> 100;
 });
-var songs =new Songs([
-  new Song({ title: "Song 1"}),
-  new Song({ title: "Song 2"}),
-  new Song({ title: "Song 3"})
-]);
-songs.add(new Song({ title: "Song 4" }));
 
+console.log("Top downloads",topDownloads);
 
-// console method check......provided by Underscore.js
-songs
-s {length: 4, models: Array(4), _byId: {…}}length: 4models: Array(4)0: s {cid: "c1", attributes: {…}, _changing: false, _previousAttributes: {…}, changed: {…}, …}1: s {cid: "c2", attributes: {…}, _changing: false, _previousAttributes: {…}, changed: {…}, …}2: s {cid: "c3", attributes: {…}, _changing: false, _previousAttributes: {…}, changed: {…}, …}3: s {cid: "c4", attributes: {…}, _changing: false, _previousAttributes: {…}, changed: {…}, …}length: 4__proto__: Array(0)_byId: {c1: s, c2: s, c3: s, c4: s}__proto__: e.Collection
-songs.at(0)
-s {cid: "c1", attributes: {…}, _changing: false, _previousAttributes: {…}, changed: {…}, …}attributes: {title: "Song 1"}changed: {}cid: "c1"collection: s {length: 4, models: Array(4), _byId: {…}}_changing: false_events: {all: Array(1)}_pending: false_previousAttributes: {}__proto__: e.Model
-songs.remove(songs.at(0));
-s {cid: "c1", attributes: {…}, _changing: false, _previousAttributes: {…}, changed: {…}, …}attributes: {title: "Song 1"}title: "Song 1"__proto__: Objectchanged: {}cid: "c1"_changing: false_events: {}_pending: false_previousAttributes: {}__proto__: e.Model
+songs.each(function(song){
+  console.log(song);
+});

dbb31730d1423c424cb35eda66fe8f74f2352538
CR <rajshekhar@protonmail.com>
2018-02-27 21:01:28 -0600

    model and new instance created

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
+
+// to create model we extend Backbone.Model.extend
+let Song = Backbone.Model.extend({
+// initialize method
+  initialize: function(){
+    console.log("A new song has been created.");
+  }
+});
+
+// instance created using new operator
+var song = new Song();

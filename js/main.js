bdbe1664ac58cf6db7aa77406409c50269061ce2
CR <rajshekhar@protonmail.com>
2018-03-02 20:09:53 -0600

    passing data view

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-// Collections
-
 var Song = Backbone.Model.extend();
+
+//passing Data to view
 var Songs = Backbone.Collection.extend({
   model:Song
-})
-
-var songs = new Songs();
-
-// by using add method, we can specifiy the index number dat where to add
-songs.add(new Song({ title: " Song 1", genre:" Jazz", downloads: 100}),{at:0});
-
-songs.push(new Song({ title: " Song 2", genre:" Jazz", downloads: 160}));
-
-var jazzSongs = songs.where({genre: "Jazz"});
-
-var firstJazzSong = songs.findWhere({genre: "Jazz"});
-
-console.log("Jazz Songs", jazzSongs);
-
-console.log("First Jazz Song", firstJazzSong);
-
-var filteredSongs = songs.where({genre: "Jazz", title: "Song 2"});
-console.log("Filtered Songs", filteredSongs);
-
-var topDownloads =songs.filter(function(song){
-  return song.get("downloads")> 100;
 });
 
-console.log("Top downloads",topDownloads);
-
-songs.each(function(song){
-  console.log(song);
+var SongView = Backbone.View.extend({
+  tagName:"li",
+  render: function(){
+    this.$el.html(this.model.get("title"));
+    return this;
+  }
 });
+
+var SongsView = Backbone.View.extend({
+  render: function(){
+    var self = this;
+
+    this.model.each(function(song){
+      var songView = new SongView({model: song});
+      self.$el.append(songView.render().$el);
+    });
+  }
+});
+
+var songs = new Songs([
+  new Song({title: "Blue in green"}),
+  new Song({title: "So what"}),
+  new Song({title:"All Blues"})
+]);
+
+var songsView = new SongsView({el: "#songs", model:songs});
+songsView.render();
